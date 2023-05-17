f50e42f72f11a04099e9026208f1020449d5d4af
iamHimanshu-vasoya <hims.vasoya@gmail.com>
2023-05-17 13:02:02 +0530

    Hv | 17-5-23 | Ch-1 Introduction to C language

---
+++ Ch-1/KM-0010.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("Name\t\t:Hemanshu patel\n");
+	printf("Father name\t:Gordhanbhai patel\n");
+	printf("Laungauge known\t:Hindi English Gujrati\n");
+	printf("Nationality\t:indian\n");
+	printf("Hobby\t\t:Cricket volleyball\n");
+	printf("Skill\t\t:Leaning Fast\n");
+	printf("Education\t:B.com\n");
+	printf("Profession\t:Toys");
+	getch();
+
+}
+
+
+
+
+
+
+
---
+++ Ch-1/KM-Q-2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("--------\n");
+	printf("l	l\n");
+	printf("R	l\n");
+	printf("N	l\n");
+	printf("W	l\n");
+	printf("l	l\n");
+	getch();
+
+}
+
+
