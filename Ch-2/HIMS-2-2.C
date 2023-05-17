cb28b41bab972578e06672034e5d9b579b1c5f6d
iamHimanshu-vasoya <hims.vasoya@gmail.com>
2023-05-17 18:21:46 +0530

    Ch-2 | 17-5-23| Ch-2 project

---
+++ Ch-2/HIMS-2-2.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#define hims printf
+main()
+{
+	const int a= 10,b=5;
+	clrscr();
+	hims("Area of Triangle: %d",a*b/2);
+	getch();
+
+}
---
+++ Ch-2/HIMS2-2-.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#define hims printf
+main()
+{
+	const int p= 100,r=5,n=2;
+	clrscr();
+	hims("Simple intrest: %d",(p*r*n)/100);
+	getch();
+
+}
